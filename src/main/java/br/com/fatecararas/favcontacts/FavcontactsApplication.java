941aaecd2a4bd1b49c566a2c47b35d803ece94a4
Esdras B Silva <bzr4@icloud.com>
2022-10-04 19:24:10 -0300

    Commit inicial

---
+++ src/main/java/br/com/fatecararas/favcontacts/FavcontactsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.favcontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavcontactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavcontactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatecararas/favcontacts/FavcontactsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.favcontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavcontactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
